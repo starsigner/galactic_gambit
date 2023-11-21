98f753bffdba07ba8cc83ea0515ce6bc54daba50
Dhara <illuviawriter@gmail.com>
2023-11-20 19:03:46 -0800

    move consts to seperate file

---
+++ constants.py
@@ -0,0 +1,12 @@
+# errors messages; constants 
+MIN_USERNAME_LEN = 1
+MAX_USERNAME_LEN = 20
+MIN_PASS_LEN = 8
+DUPLICATE_USERNAME_ERROR = "That username already exists. Please try again."
+MIN_USERNAME_ERROR = "Username must be at least 1 character."
+MAX_USERNAME_ERROR = "Please keep your username under 20 character."
+MIN_PASSWORD_ERROR = "Your password must be greater than 8 characters."
+SPECIAL_CHAR_PASSWORD_ERROR = "Your password must include at least one special character."
+NUMERIC_CHAR_PASSWORD_ERROR = "Your password must include at least one numeric character."
+SUCCESSFUL_LOGIN = "Successfully logged in."
+INCORRECT_LOGIN = "Incorrect login. Please try again. Enter 0 to return."
