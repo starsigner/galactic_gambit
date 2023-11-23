0b1023bc76fe74eaf1f3535149cf4a010ba7b51e
Dhara <illuviawriter@gmail.com>
2023-11-23 13:38:34 -0800

    update user stats

--- menu.py
+++ menu.py
@@ -4,6 +4,11 @@ from utils import *
 
 
 def printUserStats(username):
+    print("{}'s FILE".format(username).upper())
+    printStarPattern()
+    print("Date Joined: \n")
+    print("Total Money Earned: \n")
     update_user_astrobucks(c, username, 4)
-    print(get_user_astrobucks(c, username))
+    print("Astrobucks: {} \n".format(get_user_astrobucks(c, username)))
+    print("Level: ")
 
